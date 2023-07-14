470273c7efe1091ea0ed865b64c3c40e33efd984
arabahimrane <arabahimran@gmail.com>
2023-07-14 13:12:57 -0700

    commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,11 +2,15 @@
 
 int main(void) {
     char letter;
-    
+    char bigletter;
+
     for (letter = 'a'; letter <= 'z'; letter++) {
         putchar(letter);
     }
-    
+    for(bigletter = 'A'; bigletter <= 'Z'; bigletter++) {
+      putchar(bigletter);
+    }
+
     putchar('\n');
 
     return 0;
