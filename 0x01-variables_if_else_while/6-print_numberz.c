eee0d09f34229f6234ece676a13ec6d91af80151
arabahimrane <arabahimran@gmail.com>
2023-07-14 18:23:00 -0700

    commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints the numbers from 0-9.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+        int i;
+        for(i=0; i<10; i++){
+         putchar(i);
+        }
+	return (0);
+}
