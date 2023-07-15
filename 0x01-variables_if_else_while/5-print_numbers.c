29cdd2873a1fe3045cf5fb8958a87275ba0eb6af
arabahimrane <arabahimran@gmail.com>
2023-07-14 17:55:31 -0700

    commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
-    /**
-     * main - Entry point of the program
-     *
-     * Description: Prints all single-digit numbers of base 10 starting from 0,
-     *              followed by a new line.
-     *
-     * Return: Always 0 (Success)
-     */
- int main(void)
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints all single-digit numbers of base 10 starting from 0,
+ *              followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	printf("0123456789\n");
-	return (0);
+    printf("0123456789\n");
+    return (0);
 }
