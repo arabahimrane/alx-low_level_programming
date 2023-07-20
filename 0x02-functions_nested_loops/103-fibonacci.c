c43de41d2fb8e2f888624aa16e65f4263f82a7b4
arabahimrane <arabahimran@gmail.com>
2023-07-19 19:26:15 -0700

    commit

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *main - Finds and prints the sum of even-valued terms in the Fibonacci sequence.
+ *main  Finds and prints the sum of even-valued terms in the Fibonacci sequence
  *
  * Return: Always 0.
  */
