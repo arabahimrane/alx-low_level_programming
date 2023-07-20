2bb701b0ba02fd9b54f8db1a77c184601cd339b6
arabahimrane <arabahimran@gmail.com>
2023-07-19 19:04:04 -0700

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_to_98 - Prints all natural numbers from n to 98, separated by a comma and space.
+ * print_to_98 - Prints all natural numbers from n to 98.
  * @n: The starting number.
  */
 void print_to_98(int n)
