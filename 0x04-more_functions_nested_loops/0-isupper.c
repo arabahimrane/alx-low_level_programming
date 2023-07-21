9c2901da7b868b1fd02a42814e2061e342e935cb
arabahimrane <arabahimran@gmail.com>
2023-07-20 18:10:14 -0700

    commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,9 +6,10 @@
 * Description : checks for uppercase character.
 */
 
-int _isupper(int c){
-	if(c >= 'A' && c <= 'Z')
-	  return(1);
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
 	else
-	  return(0); 
+		return (0);
 }
