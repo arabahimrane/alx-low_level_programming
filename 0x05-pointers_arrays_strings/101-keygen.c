1597690d9aa61bc08a65910ab021d689e8a1a5e0
arabahimrane <arabahimran@gmail.com>
2023-07-25 07:18:56 -0700

    commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,20 +13,28 @@ int main(void)
 	int password[69];
 	int i;
 	int sum;
-
 	sum = 0;
-	i = -1;	
+	i = -1;
+
 	srand(time(NULL));
-	while (++i <= 69)
-	{
-		password[i] = rand() % 126;
-		sum += (password[i] + '0');
-		putchar(password[i] + '0');
-		if ((2772 - sum) - '0' < 78)
-		{
-			putchar(2772 - sum);
-			return (0);
-		}
-	}
+
+	while (++i < 69)
+	  {
+
+	    password[i] = rand() % 126 + 1;
+	    sum += password[i];
+	    putchar(password[i]);
+	  }
+
+
+	if ((2772 - sum) > 0 && (2772 - sum) < 127)
+	  {
+	    putchar(2772 - sum);
+	  }
+	else
+	  {
+	    putchar(rand() % 125 + 1);
+	  }
+
 	return (0);
 }
