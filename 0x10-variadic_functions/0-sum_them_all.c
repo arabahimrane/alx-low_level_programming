19fc29ae1db6742973b05ca309e847fd29a6bc30
arabahimrane <arabahimran@gmail.com>
2023-10-12 18:09:07 -0700

    commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,23 +1,18 @@
+#include <stdio.h>
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - write a func that return the sum of all its param
- * @n: input
- * Return: sum
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-
-int sum_them_all(const unsigned int n, ...)
+int main(void)
 {
-	int sum;
-	unsigned int i;
-	va_list ap;
+    int sum;
 
-	if (n == 0)
-		return (0);
-	va_start(ap, n);
-	sum = 0;
-	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
-	va_end(ap);
-	return (sum);
+    sum = sum_them_all(2, 98, 1024);
+    printf("%d\n", sum);
+    sum = sum_them_all(4, 98, 1024, 402, -1024);
+    printf("%d\n", sum);    
+    return (0);
 }
